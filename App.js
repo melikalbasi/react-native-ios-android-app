ad7f4d8188b20e031741bef145e234301f5e5cd3
Melika Kalbasi <melikalbasi@gmail.com>
2019-02-21 01:25:01 -0800

    add button for sign in

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, TextInput, View} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 
 export default class App extends Component {
@@ -17,12 +17,15 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput 
-        style={{width: 300}}
-        placeholder="Username"
-        value={this.state.placeName}
-        onChangeText={this.placeNameChangedHandler} 
-        />
+        <View style={styles.innerContainer}>
+          <TextInput
+            style={styles.placeInput}
+            placeholder="Username"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+          />
+          <Button title="Sign In" style={styles.placeButton} />
+        </View>
       </View>
     );
   }
@@ -31,8 +34,21 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 26,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  innerContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
   }
 });
