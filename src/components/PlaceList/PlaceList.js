eb2caaf202bbdcd45997aa8914b4be50b456249b
Melika Kalbasi <melikalbasi@gmail.com>
2019-02-21 02:48:56 -0800

    implement method to remove places

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -4,7 +4,7 @@ import ListItem from '../ListItem/ListItem';
 
 const PlaceList = props => {
   const placesOutput = props.places.map((place, i) => (
-    <ListItem key={i} placeName={place} onItemPressed={() => alert('Item pressed - ID: ' + i)} />
+    <ListItem key={i} placeName={place} onItemPressed={() => props.onItemDeleted(i)} />
   ));
   return (
     <View style={styles.listContainer}>{placesOutput}</View>
