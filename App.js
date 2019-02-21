436fa227e7407b8ab3da5d5ce996be5de685ba72
Melika Kalbasi <melikalbasi@gmail.com>
2019-02-21 01:33:34 -0800

    add functionality to add button

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 export default class App extends Component {
 
   state = {
-    placeName: ""
+    placeName: "",
+    places: []
   };
 
   placeNameChangedHandler = val => {
@@ -14,17 +15,39 @@ export default class App extends Component {
     });
   };
 
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+    
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
+  };
+
   render() {
+    const placesOutput = this.state.places.map(place => (
+      <Text>{place}</Text>
+    ));
     return (
       <View style={styles.container}>
         <View style={styles.innerContainer}>
           <TextInput
             style={styles.placeInput}
-            placeholder="Username"
+            placeholder="Places You Want To Go"
             value={this.state.placeName}
             onChangeText={this.placeNameChangedHandler}
           />
-          <Button title="Sign In" style={styles.placeButton} />
+          <Button 
+          title="Add" 
+          style={styles.placeButton} 
+          onPress={this.placeSubmitHandler}
+          />
+        </View>
+        <View>
+          {placesOutput}
         </View>
       </View>
     );
@@ -34,10 +57,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 26,
+    padding: 35,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "flex-start"
   },
   innerContainer: {
     width: "100%",
